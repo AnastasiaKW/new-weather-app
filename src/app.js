62e1793801d5fe1bb5cdecb70f72a8a8455d262a
AnastasiaKW <121472561+AnastasiaKW@users.noreply.github.com>
2023-04-01 23:22:51 -0400

    forecast integrating

--- src/app.js
+++ src/app.js
@@ -22,6 +22,17 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("forecast");
+}
+
+function getForecast(coordinates) {
+  let apiKey = `2bd326a60dc89a53287e446e819664df`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -47,10 +58,12 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "0ca86a6dbeb8f01289e9c396e6dc56da";
+  let apiKey = `2bd326a60dc89a53287e446e819664df`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
